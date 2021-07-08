7a785fe6a8e47a32a35d04a6e7ce88ff3aed0701
faqundo <facundo.ferro@werden.com.ar>
2021-07-08 01:51:46 -0300

    change image profile

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Image, Button, Alert, TouchableOpacity } from 'react-native';
+import * as ImagePicker from "expo-image-picker";
+import  faztImg from "./assets/fazt.jpg";
 
-export default function App() {
+const App = () => {
+
+  const [SelectedImage, setSelectedImage] = useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if(permissionResult.granted === false) {
+      alert('Permission to access camera is required');
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if(pickerResult.cancelled === true) {
+      return
+    }
+
+    setSelectedImage({localUri: pickerResult.uri})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>First expo app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello World!!</Text>
+      <Image 
+        style={styles.image} 
+        source={{uri: SelectedImage !== null ? SelectedImage.localUri : faztImg}}/>
+      {/* <Button
+        color="#000"
+        title="Start"
+        onPress={() => Alert.alert('Hello')}
+        /> */}
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Press me</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: { color: 'red', flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#00ffff' },
+  title: { fontSize: 30 },
+  image: {height: 200, width: 200, borderRadius:100},
+  button:{
+    backgroundColor:'grey',
+    margin:10,
+    padding:10,
+    fontSize:20,
   },
+  buttonText:{color:'white'}
 });
+
+export default App;
--- README.txt
+++ README.txt
@@ -1,2 +1,7 @@
 npm install -g expo-cli (solo una vez , si ya esta instalado no, es como node)
 expo init <name app>
+
+Botones basicos => Button
+Botones personalizados => TouchableOpacity
+
+checkpoint video 1:07:58
